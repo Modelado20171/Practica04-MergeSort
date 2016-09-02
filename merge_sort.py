4bf1d761033d7e57a7f24e1f6106919bd7e2a428
sainoba <sainoba@gmail.com>
2016-09-02 00:13:42 -0500

    Creación del esqueleto de la práctica

---
+++ merge_sort.py
@@ -0,0 +1,19 @@
+# Aquí es donde debe de ir su implementación del merge sort
+
+# Esta función recibe una lista y regresa una copia ordenada
+def merge_sort(lista)
+  pass
+
+
+# Tienes estríctamente prohibido borrar líneas después de este punto
+lista1 = [1,2,3,4,5,6]
+lista2 = [6,5,4,3,2,1]
+lista3 = [5,3,8,1,3,8,7,8,2,1,6,8,1,3,8]
+
+listaOrdenada1 = merge_sort(lista1)
+listaOrdenada2 = merge_sort(lista2)
+listaOrdenada3 = merge_sort(lista3)
+
+print(listaOrdenada1)
+print(listaOrdenada2)
+print(listaOrdenada3)
