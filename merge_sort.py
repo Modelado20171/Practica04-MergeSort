02c91c7a1f301000609962797a9eb9c6e0792960
Misael Saenz Flores <misa2pac@gmail.com>
2016-09-06 20:49:59 -0500

    Se agrego la funcion merge
    Esta funcion concatena las lista que van siendo ordenadas por
    la funcion "merge_sort", comprueba primero que la longitud de
    las dos listas sea mayor a cero.
    
    Despues comprueba el indice cero que sea menor que el indice
    cero de la lista derecha y lo agrega, de lo contrario hace los
    mismo pero al reves y regresa la lista concatenada.

--- merge_sort.py
+++ merge_sort.py
@@ -1,9 +1,24 @@
 # Aquí es donde debe de ir su implementación del merge sort
 
 # Esta función recibe una lista y regresa una copia ordenada
-def merge_sort(lista)
-  pass
+def merge_sort(lista):
+	pass
 
+# Definimos la funcion para concatenar las listas ya ordenadas.
+def merge(ListIzq, ListDer):
+	resultado = []
+	while(len(ListIzq) > 0 and len(ListDer) > 0):
+		if(ListIzq[0] <= ListDer[0]):
+			resultado.append(ListIzq[0])
+			ListIzq = ListIzq[1:]
+		else:
+			resultado.append(ListDer[0])
+			ListDer = ListDer[1:]
+	if(len(ListIzq) > 0):
+		resultado.extend(ListIzq[0:])
+	if(len(ListDer) > 0):
+		resultado.extend(ListDer[0:])
+	return resultado
 
 # Tienes estríctamente prohibido borrar líneas después de este punto
 lista1 = [1,2,3,4,5,6]
