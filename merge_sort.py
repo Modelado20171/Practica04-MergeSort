809da2e54521aa31f8d3fe25d4453bc568ca0c1b
pedro <pedro_merolito@hotmail.com>
2016-09-07 00:15:38 -0500

    Sin querer se subio el codigo de alguien que no conosco pero su rama empieza igual que la mia y este es mi codigo no el de el

--- merge_sort.py
+++ merge_sort.py
@@ -2,49 +2,26 @@
 
 # Esta función recibe una lista y regresa una copia ordenada
 def merge_sort(lista):
-	return divide_lista(lista)
-
-#Método que se encarga de dividir una lista en dos, hasta llegar a listas de tamaño uno (ya ordenadas) y dejándolas listas para aplicar la función merge.
-def divide_lista(lista):
 	if len(lista) == 1:
 		return lista
-	else:
-		tam_listas_divididas= len(lista)//2 #Las vamos diviendo entre dos (usando función piso si son de tamaño impar) hasta llegar a listas de tamaño 1.                                                                                                                                                                                              HOLA 
-		nueva_lista1= list()
-		nueva_lista2= list()
-		for i in range(len(lista)):
-			if i < tam_listas_divididas:
-				nueva_lista1.append(lista[i])   #Izquierda.
-			else:
-				nueva_lista2.append(lista[i])	#Derecha
-				
-		izquierda= divide_lista(nueva_lista1)
-		derecha= divide_lista(nueva_lista2)
+	lista_izquierda = lista[:len(lista)//2]
+	lista_derecha = lista[len(lista)//2:]
+	return merge(merge_sort(lista_izquierda),merge_sort(lista_derecha))
 
-		return merge(izquierda, derecha)
-
-#Método que junta las listas ordenadas de tamaño uno, comparándolas y agregándolas en una nueva lista de forma ordenada ascendiente.
-def merge(listaIzq, listaDer):
-	listaOrd= list()
-	
-	while listaIzq and listaDer:
-		if listaIzq[0] <= listaDer[0]:
-			listaOrd.append(listaIzq[0])
-			del listaIzq[0]
+def merge(lista1,lista2):
+	lista_ordenada = list()
+	indice_izq,indice_der = 0, 0
+	while indice_izq < len(lista1) and indice_der < len(lista2):
+		if lista1[indice_izq] < lista2[indice_der]:
+			lista_ordenada.append(lista1[indice_izq])
+			indice_izq += 1
 		else:
-			listaOrd.append(listaDer[0])
-			del listaDer[0]
-			
-	while listaIzq:
-		listaOrd.append(listaIzq[0])
-		del listaIzq[0]
-	while listaDer:
-		listaOrd.append(listaDer[0])
-		del listaDer[0]
-		
-	return listaOrd
-	
-# Tienes estríctamente prohibido borrar líneas después de este punto
+			lista_ordenada.append(lista2[indice_der])
+			indice_der += 1
+	lista_ordenada += lista1[indice_izq:]
+	lista_ordenada += lista2[indice_der:]
+	return lista_ordenada
+
 lista1 = [1,2,3,4,5,6]
 lista2 = [6,5,4,3,2,1]
 lista3 = [5,3,8,1,3,8,7,8,2,1,6,8,1,3,8]
