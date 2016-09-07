225645c8d6514f73de5700aed2b4f2bbd026e05e
pedro <pedro_merolito@hotmail.com>
2016-09-06 23:53:21 -0500

    implemente merge sort recursivo

--- merge_sort.py
+++ merge_sort.py
@@ -1,8 +1,26 @@
 # Aquí es donde debe de ir su implementación del merge sort
 
 # Esta función recibe una lista y regresa una copia ordenada
-def merge_sort(lista)
-  pass
+def merge_sort(lista):
+	if len(lista) == 1:
+		return lista
+	lista_izquierda = lista[:len(lista)//2]
+	lista_derecha = lista[len(lista)//2:]
+	return merge(merge_sort(lista_izquierda),merge_sort(lista_derecha))
+
+def merge(lista1,lista2):
+	lista_ordenada = list()
+	indice_izq,indice_der = 0, 0
+	while indice_izq < len(lista1) and indice_der < len(lista2):
+		if lista1[indice_izq] < lista2[indice_der]:
+			lista_ordenada.append(lista1[indice_izq])
+			indice_izq += 1
+		else:
+			lista_ordenada.append(lista2[indice_der])
+			indice_der += 1
+	lista_ordenada += lista1[indice_izq:]
+	lista_ordenada += lista2[indice_der:]
+	return lista_ordenada
 
 
 # Tienes estríctamente prohibido borrar líneas después de este punto
