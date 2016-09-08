43f3ab1b23884388dc5f0a72f604063a457a2b28
Noe Chavez <noechavez0001@gmail.com>
2016-09-08 09:14:28 -0500

    creada funcion merge_sort y funcion merge

--- merge_sort.py
+++ merge_sort.py
@@ -1,11 +1,34 @@
-# Aquí es donde debe de ir su implementación del merge sort
+#!/usr/bin/python
 
-# Esta función recibe una lista y regresa una copia ordenada
+#-*- coding: utf-8 -*-
+
+# Aqui es donde debe de ir su implementacion del merge sort
+
+# Esta funcion recibe una lista y regresa una copia ordenada
 def merge_sort(lista):
-    pass
+	if len(lista) == 1:
+		return lista;
+	m = len(lista)//2
+	left = lista[:m]
+	right = lista[m:]
+	left = merge_sort(left)
+	right = merge_sort(right)
+	return list(merge(left, right))
 
-
-# Tienes estríctamente prohibido borrar líneas después de este punto
+def merge(left, right):
+    result = []
+    i ,j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+# Tienes estrictamente prohibido borrar lineas despues de este punto
 lista1 = [1,2,3,4,5,6]
 lista2 = [6,5,4,3,2,1]
 lista3 = [5,3,8,1,3,8,7,8,2,1,6,8,1,3,8]
