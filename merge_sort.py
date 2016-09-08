f41c8a7d1188218207a9fbfb5ea828df5b20d345
edervs <eedervs.97@gmail.com>
2016-09-08 12:40:06 -0500

    MergeSort completado.

--- merge_sort.py
+++ merge_sort.py
@@ -1,8 +1,20 @@
 # Aquí es donde debe de ir su implementación del merge sort
+def merge(l1, l2):
+    if l1 == []:
+        return l2
+    if l2 == []:
+        return l1
+    if l1[0] < l2[0]:
+        return [l1[0]]+merge(l1[1:], l2)
+    return [l2[0]]+merge(l1, l2[1:])
 
 # Esta función recibe una lista y regresa una copia ordenada
 def merge_sort(lista):
-    pass
+    if lista == [] or len(lista) == 1:
+        return lista
+    izq = lista[:int(len(lista)/2)]
+    der = lista[int(len(lista)/2):]
+    return merge(merge_sort(izq), merge_sort(der))
 
 
 # Tienes estríctamente prohibido borrar líneas después de este punto
