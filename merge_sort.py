2dc48cb5a7c87444930dc1a8f5d1037ba1e729f6
Arturo Castillo <acv629@gmail.com>
2016-09-08 15:59:10 -0500

    Se acabó de implementar merge_sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,27 +1,38 @@
 # Aquí es donde debe de ir su implementación del merge sort
 
 # Esta función recibe una lista y regresa una copia ordenada
+import math
+
 def merge_sort(lista):
-	if len(lista) == 1:
+	if len(lista)==1:
 		return lista
-	lista_izquierda = lista[:len(lista)//2]
-	lista_derecha = lista[len(lista)//2:]
-	return merge(merge_sort(lista_izquierda),merge_sort(lista_derecha))
+	mitadLista= math.floor(len(lista)/2)
+	listaIzquierda=lista[:mitadLista]
+	listaDerecha=lista[mitadLista:]
+	return merge(merge_sort(listaIzquierda), merge_sort(listaDerecha))
 
-def merge(lista1,lista2):
-	lista_ordenada = list()
-	indice_izq,indice_der = 0, 0
-	while indice_izq < len(lista1) and indice_der < len(lista2):
-		if lista1[indice_izq] < lista2[indice_der]:
-			lista_ordenada.append(lista1[indice_izq])
-			indice_izq += 1
+
+def merge(listaIzquierda, listaDerecha):
+	i=0
+	j=0
+	lonIzq=len(listaIzquierda)
+	lonDer= len(listaDerecha)
+	resultado=[]
+
+	while i<lonIzq and j< lonDer:
+		if listaIzquierda[i]<listaDerecha[j]:
+			resultado.append(listaIzquierda[i])
+			i=i+1
 		else:
-			lista_ordenada.append(lista2[indice_der])
-			indice_der += 1
-	lista_ordenada += lista1[indice_izq:]
-	lista_ordenada += lista2[indice_der:]
-	return lista_ordenada
-
+			resultado.append(listaDerecha[j])
+			j=j+1
+	while i<lonIzq:
+		resultado.append(listaIzquierda[i])
+		i=i+1
+	while j<lonDer:
+		resultado.append(listaDerecha[j])
+		j=j+1
+	return resultado
 
 # Tienes estríctamente prohibido borrar líneas después de este punto
 lista1 = [1,2,3,4,5,6]
