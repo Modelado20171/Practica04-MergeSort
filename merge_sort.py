65b10d6a4d59e19b68262bc9482de6cbd9c3bf3d
AndresBarbosa <andresbob@ciencias.unam.mx>
2016-09-09 00:15:25 -0500

    MergeSort implementado

--- merge_sort.py
+++ merge_sort.py
@@ -1,10 +1,37 @@
-# Aquí es donde debe de ir su implementación del merge sort
-
 # Esta función recibe una lista y regresa una copia ordenada
 def merge_sort(lista):
-    pass
+    return split(lista)
 
-
+#Va a partir la lista hasta que solo tengan un elemento.
+def split(lista):
+    if len(lista) == 1:
+        return lista
+    else:
+        mitad = len(lista)//2
+        mitadIzquierda = lista[:mitad]
+        mitadDerecha = lista[mitad:]
+    return merge(split(mitadIzquierda), split(mitadDerecha))
+        
+#Va a comparar cada elemento de la lista y ordenarla        
+def merge(mitadIzquierda, mitadDerecha):
+    
+    listaOrdenada = list()
+    i=j=0    
+    while i < len(mitadIzquierda) and j < len(mitadDerecha):
+        if mitadIzquierda[i] < mitadDerecha[j]:
+            listaOrdenada.append(mitadIzquierda[i])
+            i = i + 1
+        else:
+            listaOrdenada.append(mitadDerecha[j])
+            j = j + 1
+    while i < len(mitadIzquierda):
+        listaOrdenada.append(mitadIzquierda[i])
+        i=i+1
+    while j < len(mitadDerecha):
+        listaOrdenada.append(mitadDerecha[j])
+        j=j+1
+        
+    return listaOrdenada
 # Tienes estríctamente prohibido borrar líneas después de este punto
 lista1 = [1,2,3,4,5,6]
 lista2 = [6,5,4,3,2,1]
