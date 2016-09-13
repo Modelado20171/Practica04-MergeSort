b66d8e0884645fa0c21aa6ccd9d930ee70fcd5d9
Lezama Hernández María Ximena <lezama@ciencias.unam.mx>
2016-09-12 22:09:25 -0500

    agregue mis correciones

--- merge_sort.py
+++ merge_sort.py
@@ -11,18 +11,18 @@ def mezcla(lista1, lista2):
 			aux.append(lista2[indice2])
 			indice2 += 1
 
-		if indice1 == len(l1):
+		if indice1 == len(lista1):
 			aux.extend(lista2[indice2:])
     	elif indice2 == len(lista2):
-    		aux.extend(l1[indice1:])
-        return l
-        
+    		aux.extend(lista1[indice1:])
+        return aux
+
 # Esta función recibe una lista y regresa una copia ordenada
-def merge_sort(lista)
+def merge_sort(lista):
   if len(lista) < 2:
     return lista
-  half = len(lista) // 2
-  return merge(merge_sort(lista[:half]), merge_sort(lista[half:]))
+half = len(lista) // 2
+  return mezcla(merge_sort(lista[:half]), merge_sort(lista[half:]))
 
 
 # Tienes estríctamente prohibido borrar líneas después de este punto
