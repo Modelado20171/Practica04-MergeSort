2847f0875697c20ef5e2fb558d34cf2d6f34f860
NoraLuna <nora-hdz@ciencias.unam.mx>
2016-09-13 16:56:47 -0500

    Agregue la funcion merge sort y una auxiliar

--- merge_sort.py
+++ merge_sort.py
@@ -2,8 +2,41 @@
 
 # Esta función recibe una lista y regresa una copia ordenada
 def merge_sort(lista):
-    pass
-
+	if len(lista) < 2 :
+		return lista #Si la lista tiene menos de dos elementos, regresa la lista
+	else:
+		#Dividimos la lista
+		mitad = len(lista) // 2
+		#variable auxiliar para guardar 
+		aux = []
+		aux = divide_lista(merge_sort(lista[:mitad]), merge_sort(lista[mitad:])) 
+		return aux
+#Creamos una funcion para dividir la lista
+def divide_lista(lista1, lista2):
+	lisAyuda = []
+	#creamos variables para movernos en las listas
+	ind1 = 0
+	ind2 = 0
+	#iteramos en las listas
+	while(ind1 < len(lista1) and ind2 < len(lista2)):
+		#Hacemos la comparacion entre los indices
+		if(lista1[ind1] < lista2[ind2]):
+			#agregamos el indice1 de la lista1 a la lista auxiliar
+			lisAyuda.append(lista1[ind1])
+			ind1 += 1
+		else:
+			#agregamos el indice2 a la lista auxiliar
+			lisAyuda.append(lista2[ind2])
+			ind2 +=1
+		#verificamos 
+		if(ind1 == len(lista1)):
+			#con extend añade el contenido de la lista2 a la lista auxiliar
+			lisAyuda.extend(lista2[ind2:])
+		else:
+			if(ind2 == len(lista2)):
+				#añade el contenido de la lista1 a la lista auxiliar
+				lisAyuda.extend(lista1[ind1:])
+	return lisAyuda
 
 # Tienes estríctamente prohibido borrar líneas después de este punto
 lista1 = [1,2,3,4,5,6]
