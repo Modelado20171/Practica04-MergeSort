6b77b19a6d51924c516cc49fc255b55d7067cebd
ianArlen <ian.street.fut@gmail.com>
2016-09-29 18:26:16 -0500

    Implemente merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,10 +1,35 @@
-# Aquí es donde debe de ir su implementación del merge sort
 
-# Esta función recibe una lista y regresa una copia ordenada
+def ordena(lista1, lista2):
+	""" Nos ordena la lista en dos sublistas 
+    	  Recibe lista1 y lista2 las cuales seran mezcladas.
+      	 Devuelve: Una lista con la union de la lista1 y lista 2"""	
+	posicion1 = 0 # Posicion o indice de la lista. 
+	posicion2 = 0 # Posicion o indice de la lista. 
+	guarda = [] #Auxiliar que guarda  el resultado de las dos sublistas. 
+	while(posicion1 < len(lista1) and posicion2 < len(lista2)):
+		if(lista1[posicion1] < lista2[posicion2]):
+			guarda.append(lista1[posicion1]) #Se guarda mayores y menores en las listas (de una sublista). 
+			posicion1 += 1
+		else:
+			guarda.append(lista2[posicion2]) #Se guarda mayores y menores en las listas (de una sublista).
+			posicion2 +=1
+		if(posicion1 == len(lista1)):# Las sublistas ahora se convierten en una lista de guarda. 
+			guarda.extend(lista2[posicion2:])
+		else:
+			if(posicion2 == len(lista2)):# Las sublistas ahora se convierten en una lista de guarda.
+				guarda.extend(lista1[posicion1:])
+	return guarda
 def merge_sort(lista):
-    pass
-
-
+	"""  Metodo auxiliar que nos devuelve una lista y verifica su tamanio 
+    	  Recibe lista para ordenar.
+      	 Devuelve: lista ordenada"""	
+	if len(lista) < 2 :# Si la lista es menor a 1, no hay nada que ordenar.  
+		return lista 
+	else:
+		mitad = len(lista) // 2 
+		temp = [] # Lista auxiliar. 
+		temp = ordena(merge_sort(lista[:mitad]), merge_sort(lista[mitad:])) # Separa la lista original en mitades.   
+		return temp
 # Tienes estríctamente prohibido borrar líneas después de este punto
 lista1 = [1,2,3,4,5,6]
 lista2 = [6,5,4,3,2,1]
